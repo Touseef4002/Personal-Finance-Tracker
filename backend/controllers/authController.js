993fd06cc695793116eb693592f236bbc7dd0959
Touseef4002 <sufialam55@gmail.com>
2025-07-25 01:15:01 +0530

    test

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -10,7 +10,43 @@ const generateToken = (id) => {
 
 // Register a new user
 exports.registerUser = async (req, res) => {
-    
+    const { name, email, password, profileImageUrl} = req.body;
+
+    // Validate input
+    if (!name || !email || !password) {
+        return res.status(400).json({ message: "Please fill all fields" });
+    }
+
+    try {
+        // Check if user already exists
+        const existingUser = await User.findOne({email});
+        if (existingUser) {
+            return res.status(400).json({ message: "User already exists" });
+        }
+
+        // Create new user
+        const user = new User({
+            name,
+            email,
+            password,
+            profileImageUrl
+        });
+
+        res.status(201).json({
+            message: "User registered successfully",
+            user: {
+                id: user._id,
+                name: user.name,
+                email: user.email,
+                profileImageUrl: user.profileImageUrl
+            },
+            token: generateToken(user._id)
+        });
+    }
+    catch (error) {
+        console.error("Error registering user:", error);
+        res.status(500).json({ message: "Server error" });
+    }
 }
 
 //Login a user
