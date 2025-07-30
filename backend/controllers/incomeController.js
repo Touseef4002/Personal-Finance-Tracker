c5612567c03b3330e3f5e68d2f25d459472c92de
Touseef4002 <sufialam55@gmail.com>
2025-07-30 22:58:16 +0530

    addIncome done

--- backend/controllers/incomeController.js
+++ backend/controllers/incomeController.js
@@ -1,7 +1,33 @@
 const User = require("../models/userModel");
+const Income = require("../models/Income");
 
 //Add Income Source
-exports.addIncome = async (req, res) => {}
+exports.addIncome = async (req, res) => {
+    const userId = req.user.id;
+
+    try{
+        const { icon, source, amount, date } = req.body;
+
+        if(!source || !amount || !date) {
+            return res.status(400).json({ message: "Please fill all the fields" });
+        }
+
+        const newIncome = new Income({
+            userId,
+            icon,
+            source,
+            amount,
+            date: new Date(date)
+        });
+
+        await newIncome.save();
+        res.status(201).json({ message: "Income source added successfully", income: newIncome });
+    }
+    catch (error) {
+        console.error("Error adding income:", error);
+        return res.status(500).json({ message: "Internal server error" });
+    }
+}
 
 //Get All Incomes
 exports.getAllIncome = async (req, res) => {}
