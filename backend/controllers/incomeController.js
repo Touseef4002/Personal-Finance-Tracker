09d945d56214de6d47d5d23fa06d2eab98864d9b
Touseef4002 <sufialam55@gmail.com>
2025-08-01 23:55:05 +0530

    income routes cont.

--- backend/controllers/incomeController.js
+++ backend/controllers/incomeController.js
@@ -30,7 +30,18 @@ exports.addIncome = async (req, res) => {
 }
 
 //Get All Incomes
-exports.getAllIncome = async (req, res) => {}
+exports.getAllIncome = async (req, res) => {
+    const userId = req.user.id;
+
+    try{
+        const income = await Income.find({ userId }).sort({ date: -1 });
+        res.status(200).json(income);
+    }
+    catch (error) {
+        console.error("Error fetching incomes:", error);
+        return res.status(500).json({ message: "Internal server error" });
+    }
+}
 
 //Delete Income Source
 exports.deleteIncome = async (req, res) => {}
