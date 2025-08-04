da1e602a6c13546aa48f18a2445e2287b23d0771
Touseef4002 <sufialam55@gmail.com>
2025-08-04 22:51:43 +0530

    started dashboard

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ const connectDB = require("./config/db");
 const authRoutes = require("./routes/authRoutes");
 const incomeRoutes = require("./routes/incomeRoutes");
 const expenseRoutes = require("./routes/expenseRoutes"); 
+const dashboardRoutes = require("./routes/dashboardRoutes");
 
 const app = express();
 
@@ -21,7 +22,8 @@ connectDB();
 
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/income", incomeRoutes);
-app.use("/api/v1/income", expenseRoutes);
+app.use("/api/v1/expense", expenseRoutes);
+app.use("/api/v1/dashboard", dashboardRoutes);
 
 //Serve uoploads folder 
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
